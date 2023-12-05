5d19c0a3810d99f1cf364b43a73b7fa0c070424d
mbk7368 <mbk7368@gmail.com>
2023-12-05 16:15:14 +0300

    H

--- Feedback-integration.py
+++ Feedback-integration.py
@@ -1,1 +1,19 @@
-#! /usr/bin/env python3
+##! /usr/bin/env python3
+
+import os
+import requests
+
+file_names = os.listdir("C:\\Users\\mbk73\\Desktop\\DD\\")
+file_sub_path = "C:\\Users\\mbk73\\Desktop\\DD\\"
+file_paths = []
+data_dic = {}
+
+for file_name in file_names:
+    file_appending = file_sub_path+str(file_name)
+    file_paths.append(file_appending)
+
+for file_path in file_paths:
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        lines = [line.strip() for line in lines]
+        print(lines)
