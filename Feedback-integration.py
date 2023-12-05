34f29a5d3f749c9739bb2458a111e726df1d2b25
mbk7368 <mbk7368@gmail.com>
2023-12-06 02:48:49 +0300

    H

--- Feedback-integration.py
+++ Feedback-integration.py
@@ -5,6 +5,8 @@ import requests
 
 file_names = os.listdir("C:\\Users\\mbk73\\Desktop\\DD\\")
 file_sub_path = "C:\\Users\\mbk73\\Desktop\\DD\\"
+url = "http://<corpweb-external-IP>/feedback"
+
 file_paths = []
 data_dic = {}
 
@@ -16,4 +18,15 @@ for file_path in file_paths:
     with open(file_path, 'r') as file:
         lines = file.readlines()
         lines = [line.strip() for line in lines]
-        print(lines)
+        title = lines[0]
+        name = lines[1]
+        date = lines[2]
+        comment = lines[3]
+        file_data = {'title':title, 'name':name,'date':date,'comment':comment}
+        data_dic[file_path] = file_data
+        response = requests.post(url, json=data_dic)
+
+if response.status_code == 200:
+    print("Data sent successfully")
+else:
+    print("Error sending data, status code:"+ response.status_code)
