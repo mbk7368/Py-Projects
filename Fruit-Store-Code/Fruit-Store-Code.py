4d2c914720ebd7fd186463e0ae14a33f037898b2
mbk7368 <mbk7368@gmail.com>
2023-12-12 16:18:23 +0300

    a

---
+++ Fruit-Store-Code/Fruit-Store-Code.py
@@ -0,0 +1,12 @@
+"""
+create the following and test each function seperately before the final integaration.
+1- a function to convert images from tif to jpeg and modify their size.
+2- a function that transforms txt file into an html file and include the image and the description.
+3- a function to upload the html file to a web service that is using django.
+4- a function to gather the name and weight of all fruit from the txt file and upload it to the web server using python requests.
+5- a function that measure the total weight of fruit uploaded to the server and store them in a list of lists.
+6- a function that generate a pdf report containing the measure of the total weight of fruit uploaded to the server and their names.
+7- a function that sends an email to the supplier to notify them of the names and weight of the fruit uploaded to the website and 
+   attach a pdf that contain the information.
+8- a function that checks the health of the system and sends an email if something went wrong.
+"""
