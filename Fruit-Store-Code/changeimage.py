ac2b80809d3a987c1a30ebc027c2777267d29098
mbk7 <mbk7368@gmail.com>
2023-12-12 18:18:33 +0300

    l

--- Fruit-Store-Code/changeimage.py
+++ Fruit-Store-Code/changeimage.py
@@ -1,33 +1,31 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
 
 from PIL import Image
 import os
 
+#directory = '~/supplier-data/images'
+directory = 'C:\\temp1\\images\\'
+
+"""take a list of images, transfom them to .jpeg format and save them to their original folder"""
 def process_images(directory):
     # Get the list of files in the directory
     file_list = os.listdir(directory)
-
     for filename in file_list:
         if filename.endswith('.tiff'):
             # Construct the file paths for processing
             image_path = os.path.join(directory, filename)
             output_filename = os.path.splitext(filename)[0] + '.jpeg'
             output_path = os.path.join(directory, output_filename)
-
             # Open the image
             image = Image.open(image_path)
-
             # Convert RGBA to RGB if necessary
             if image.mode == 'RGBA':
                 image = image.convert('RGB')
-
             # Resize the image
             resized_image = image.resize((600, 400))
-
             # Save the processed image as JPEG
             resized_image.save(output_path, 'JPEG')
             print(f"Image processed and saved as: {output_path}")
-
 # Example usage
-directory = '~/supplier-data/images'
+
 process_images(directory)
