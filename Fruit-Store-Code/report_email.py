624683fef9b40c80355dca6bebc6b6e64e1fc86a
mbk7368 <mbk7368@gmail.com>
2023-12-14 23:56:39 +0300

    s

--- Fruit-Store-Code/report_email.py
+++ Fruit-Store-Code/report_email.py
@@ -2,32 +2,6 @@
 
 import os
 from datetime import date
-<<<<<<< HEAD
-from reports import generate_report
-
-folder_path = "/home/student/supplier-data/descriptions/"
-
-def process_data(folder_path):
-    fruit_data = []
-    output = []
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".txt"):
-            file_path = os.path.join(folder_path, file_name)
-            with open(file_path, "r") as file:
-                lines = file.readlines()
-                name = lines[0].strip()
-                weight = lines[1].strip()
-                fruit_data.append({"name": name, "weight": weight})
-    for item in fruit_data:
-        output += "\n\n"
-        output += f"name: {item['name']}\n"
-        output += f"weight: {item['weight']}\n"
-        output += "\n"
-    return output
-
-def main():
-    generate_report("/tmp/processed.pdf")
-=======
 import reports
 from emails import send_email
 from reports import read_text_files
@@ -53,7 +27,6 @@ def main():
     body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
     attachment_path = "/tmp/processed.pdf"
     send_email(sender, recipient, subject, body, attachment_path)
->>>>>>> 4c04b0ea04c4b4aa0540937f66eead695e2877ae
 
 if __name__ == "__main__":
     main()
